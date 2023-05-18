f5f4cad4d1529e9982706ab64f92d452e0bfb8ea
evshiron <evshiron@gmail.com>
2023-05-18 11:40:13 +0800

    chore: store torchvision wheels to releases

--- scripts/build_torchvision.sh
+++ scripts/build_torchvision.sh
@@ -10,8 +10,8 @@ export FORCE_CUDA=1
 
 # build torchvision
 
-curl -L -O https://pub-1cbfe09f357e4aa1a82dc7a43cc443ab.r2.dev/wheel/torch-2.0.1%2Bgitd0d0524-cp310-cp310-linux_x86_64.whl
-pip install torch-2.0.1%2Bgitd0d0524-cp310-cp310-linux_x86_64.whl
+curl -L -O https://github.com/evshiron/rocm_lab/releases/download/v1.14.514/torch-2.0.1+gite19229c-cp310-cp310-linux_x86_64.whl
+pip install torch-2.0.1+gite19229c-cp310-cp310-linux_x86_64.whl
 
 curl -L -O https://github.com/pytorch/vision/archive/refs/tags/v0.15.2.tar.gz
 tar -xzvf v0.15.2.tar.gz

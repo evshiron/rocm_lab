a49c2c3092abe0da4b2753ca73d64a2a5b607b3e
evshiron <evshiron@gmail.com>
2023-05-25 12:02:46 +0800

    chore: update build script for bitsandbytes

--- scripts/build_bitsandbytes.sh
+++ scripts/build_bitsandbytes.sh
@@ -6,21 +6,16 @@ python3 -m venv venv
 
 source venv/bin/activate
 
-curl -O https://pub-1cbfe09f357e4aa1a82dc7a43cc443ab.r2.dev/wheel/torch-2.0.1%2Bgitd0d0524-cp310-cp310-linux_x86_64.whl
-pip install torch-2.0.1%2Bgitd0d0524-cp310-cp310-linux_x86_64.whl
-
-curl -O https://pub-1cbfe09f357e4aa1a82dc7a43cc443ab.r2.dev/wheel/torchvision-0.15.2%2B6770a25-cp310-cp310-linux_x86_64.whl
-pip install torchvision-0.15.2%2B6770a25-cp310-cp310-linux_x86_64.whl
-  
-export CUDA_VERSION=gfx1100
+export ROCM_HOME=/opt/rocm-5.5.0
+export HCC_AMDGPU_TARGET=gfx1100
 
 # build bitsandbytes-rocm
 
-git clone https://github.com/evshiron/bitsandbytes-rocm
+git clone https://github.com/agrocylo/bitsandbytes-rocm
 
 cd bitsandbytes-rocm
 
-pip install setuptools wheel cmake ninja
+pip install setuptools wheel
 
 make hip
 python setup.py bdist_wheel

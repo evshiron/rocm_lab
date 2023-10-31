38c87d8bc20b89371a30282a614873924aab46a3
Johnny <johnnync13@gmail.com>
2023-10-31 01:38:45 +0100

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,7 +2,7 @@
 
 apt update && apt install -y git vim curl build-essential python3-pip python3-venv libpython3-dev libpng-dev libjpeg-dev
 
-curl -O https://repo.radeon.com/amdgpu-install/5.5/ubuntu/jammy/amdgpu-install_5.5.50500-1_all.deb
-dpkg -i amdgpu-install_5.5.50500-1_all.deb
+curl -O https://repo.radeon.com/amdgpu-install/5.7/ubuntu/jammy/amdgpu-install_5.7.50700-1_all.deb
+dpkg -i amdgpu-install_5.7.50700-1_all.deb
 
 DEBIAN_FRONTEND=noninteractive amdgpu-install --usecase=rocm --no-dkms --no-32 -y

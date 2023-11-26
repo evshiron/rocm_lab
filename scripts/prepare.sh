484d4e40245281658a67821963289b73d69d7fff
Johnny <johnnync13@gmail.com>
2023-11-26 18:15:17 +0100

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,7 +7,7 @@ sudo apt-get update && sudo apt-get install -y \
   clang-format \
   clang-tidy \
   cmake
-curl -O https://repo.radeon.com/amdgpu-install/5.7.1/ubuntu/jammy/amdgpu-install_5.7.50701-1_all.deb
-dpkg -i amdgpu-install_5.7.50701-1_all.deb
+curl -O https://repo.radeon.com/amdgpu-install/6.0.0/ubuntu/jammy/amdgpu-install_6.0.0-1_all.deb
+dpkg -i amdgpu-install_6.0.0-1_all.deb
 
 DEBIAN_FRONTEND=noninteractive amdgpu-install --usecase=rocm --no-dkms --no-32 -y

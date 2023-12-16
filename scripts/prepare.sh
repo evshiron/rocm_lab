7b6d468774f9f4059aae4a5ff3c09a10a5b7f3e6
Johnny <johnnync13@gmail.com>
2023-12-16 20:17:07 +0100

    rocm6

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,7 +7,7 @@ sudo apt-get update && sudo apt-get install -y \
   clang-format \
   clang-tidy \
   cmake
-curl -O https://repo.radeon.com/amdgpu-install/6.0.0/ubuntu/jammy/amdgpu-install_6.0.0-1_all.deb
-dpkg -i amdgpu-install_6.0.0-1_all.deb
+curl -O https://repo.radeon.com/amdgpu-install/6.0/ubuntu/jammy/amdgpu-install_6.0.60000-1_all.deb
+dpkg -i amdgpu-install_6.0.60000-1_all.deb
 
-DEBIAN_FRONTEND=noninteractive amdgpu-install --usecase=rocm --no-dkms --no-32 -y
+DEBIAN_FRONTEND=noninteractive amdgpu-install --usecase=graphics,rocm -y

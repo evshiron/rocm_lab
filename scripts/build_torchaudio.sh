d95f15738e6dc40e3b98dc8483cca14338dedb33
Johnny <johnnync13@gmail.com>
2023-12-17 11:29:16 +0100

    Update build_torchaudio.sh

--- scripts/build_torchaudio.sh
+++ scripts/build_torchaudio.sh
@@ -6,23 +6,20 @@ python3 -m venv venv
 
 source venv/bin/activate
 
-export ROCM_PATH=/opt/rocm-5.5.0
+export ROCM_PATH=/opt/rocm-6.0.0
 export PYTORCH_ROCM_ARCH=gfx1100
 
 # build torchvision
 
-curl -L -O https://github.com/evshiron/rocm_lab/releases/download/v1.14.514/torch-2.0.1+gite19229c-cp310-cp310-linux_x86_64.whl
-pip install torch-2.0.1+gite19229c-cp310-cp310-linux_x86_64.whl
+git clone --recursive -b release/2.2 https://github.com/pytorch/audio torchaudio
 
-git clone -b v2.0.2 https://github.com/pytorch/audio audio-2.0.2
-
-cd audio-2.0.2
+cd torchaudio
 git reset --hard
 # not needed for this version
 #echo 2.0.2 > version.txt
 
 # this file is required but is missing in rocm distribution
-echo 5.5.0-63 | sudo tee -a $ROCM_PATH/.info/version-dev
+echo 6.0.0 | sudo tee -a $ROCM_PATH/.info/version-dev
 
 pip install setuptools wheel cmake ninja 
 

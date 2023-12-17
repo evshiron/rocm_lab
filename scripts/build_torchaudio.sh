3f14d0dcb2d0d009b2cd581d0fb6d0fb3e9f211c
Johnny <johnnync13@gmail.com>
2023-12-17 11:29:26 +0100

    Update build_torchaudio.sh

--- scripts/build_torchaudio.sh
+++ scripts/build_torchaudio.sh
@@ -6,24 +6,16 @@ python3 -m venv venv
 
 source venv/bin/activate
 
-export ROCM_PATH=/opt/rocm-6.0.0
-export PYTORCH_ROCM_ARCH=gfx1100
+export FORCE_CUDA=1
 
 # build torchvision
 
-git clone --recursive -b release/2.2 https://github.com/pytorch/audio torchaudio
+git clone --recursive -b release/0.16 https://github.com/pytorch/vision.git torchvision
 
-cd torchaudio
-git reset --hard
-# not needed for this version
-#echo 2.0.2 > version.txt
-
-# this file is required but is missing in rocm distribution
-echo 6.0.0 | sudo tee -a $ROCM_PATH/.info/version-dev
+cd torchvision
+echo 0.16 > version.txt
 
 pip install setuptools wheel cmake ninja 
-
-git apply ../patches/torchaudio.diff
 
 python setup.py bdist_wheel
 

1e67b87e6bd72f08372586d624822a91dcf34d33
Johnny <johnnync13@gmail.com>
2024-02-05 10:33:07 +0100

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-sudo apt-get update && apt-get install -y git vim curl build-essential python3-pip python3-venv libpython3-dev libpng-dev libjpeg-dev
-sudo apt-get update && sudo apt-get install -y \
-  build-essential \
-  clang \
-  clang-format \
-  clang-tidy \
-  cmake
+sudo apt-get update && sudo apt-get install -y git vim curl build-essential python3-pip python3-venv libpython3-dev libpng-dev libjpeg-dev clang clang-format clang-tidy cmake
+
 curl -O https://repo.radeon.com/amdgpu-install/6.0.2/ubuntu/jammy/amdgpu-install_6.0.60002-1_all.deb
 dpkg -i amdgpu-install_6.0.60002-1_all.deb
 
